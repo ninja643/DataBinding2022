af399e417a5030a188de8d0427328c4a4455e5f6
Marko Milosevic <markom@niri.rs>
2022-03-29 16:36:32 +0200

    Initial commit

---
+++ app/src/main/java/rs/ac/ni/pmf/databinding2022/MainActivity.java
@@ -0,0 +1,45 @@
+package rs.ac.ni.pmf.databinding2022;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import rs.ac.ni.pmf.databinding2022.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements UsersHandler {
+
+    private ActivityMainBinding _binding;
+
+    private UsersRepository _usersRepository = UsersRepository.INSTANCE;
+    private int _currentUser = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        _binding.setUser(_usersRepository.getUser(_currentUser));
+        _binding.setUsers(_usersRepository.getUsers());
+        _binding.setHandlers(this);
+    }
+
+    @Override
+    public void nextUser() {
+
+        if (_currentUser < _usersRepository.count() - 1) {
+            _currentUser++;
+            _binding.setUser(_usersRepository.getUser(_currentUser));
+        }
+    }
+
+    @Override
+    public void previousUser() {
+        if (_currentUser > 0) {
+            _currentUser--;
+            _binding.setUser(_usersRepository.getUser(_currentUser));
+        }
+    }
+}
---
+++ app/src/main/java/rs/ac/ni/pmf/databinding2022/UsersHandler.java
@@ -0,0 +1,8 @@
+package rs.ac.ni.pmf.databinding2022;
+
+import android.view.View;
+
+public interface UsersHandler {
+    void nextUser();
+    void previousUser();
+}
